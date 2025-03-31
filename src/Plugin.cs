9f2338f91b93f3b3abb25aba1e14d9a3e1d25dc4
Cole Strain <colestrain2000@gmail.com>
2025-03-31 12:12:41 -0700

    Expedition Menu's Jukebox is now the JukeboxAnywhere version

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -14,7 +14,7 @@ using UnityEngine;
 
 namespace JukeboxAnywhere
 {
-    [BepInPlugin(MOD_ID, "Jukebox Anywhere", "1.5.0")]
+    [BepInPlugin(MOD_ID, "Jukebox Anywhere", "1.6.0")]
     class Plugin : BaseUnityPlugin
     {
         public const string MOD_ID = "olaycolay.jukeboxanywhere";
@@ -55,6 +55,8 @@ namespace JukeboxAnywhere
 
             new Hook(typeof(Page).GetProperty(nameof(Page.Selected), System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).GetGetMethod(), Page_get_Selected);
 
+            On.ProcessManager.PostSwitchMainProcess += ProcessManager_PostSwitchMainProcess;
+
             try
             {
                 IL.Menu.ExpeditionJukebox.ctor += ExpeditionJukebox_ctor;
@@ -63,6 +65,18 @@ namespace JukeboxAnywhere
             {
                 Debug.LogError("JukeboxAnywhere: Could not apply ExpeditionJukebox_ctor IL Hook!\n" + ex.Message);
             }
+        }
+
+        private void ProcessManager_PostSwitchMainProcess(On.ProcessManager.orig_PostSwitchMainProcess orig, ProcessManager self, ProcessManager.ProcessID ID)
+        {
+            if (ID == ExpeditionEnums.ProcessID.ExpeditionJukebox)
+            {
+                self.currentMainLoop = new JukeboxAnywhere(self);
+                orig(self, null);
+                return;
+            }
+
+            orig(self, ID);
         }
 
         private delegate bool orig_Menu_FreezeMenuFunctions(Menu.Menu self);
